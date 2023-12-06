80e19e7d452e3b8218b37e9282b5b8e54d988208
Tom Hamming <tom.hamming@gmail.com>
2023-12-06 14:55:46 -0800

    Add files

---
+++ MXSymbolicate.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+import json
+import os, sys
+import subprocess
+import re
+import datetime
+import argparse
+
+# From mac/exception_types.h, as per https://developer.apple.com/documentation/metrickit/mxcrashdiagnostic/3552297-exceptiontype?language=objc
+exceptionTypes = {1: "EXC_BAD_ACCESS",
+                    2: "EXC_BAD_INSTRUCTION",
+                    3: "EXC_ARITHMETIC",
+                    4: "EXC_EMULATION",
+                    5: "EXC_SOFTWARE",
+                    6: "EXC_BREAKPOINT",
+                    7: "EXC_SYSCALL",
+                    8: "EXC_MACH_SYSCALL",
+                    9: "EXC_RPC_ALERT",
+                    10: "EXC_CRASH",
+                    11: "EXC_RESOURCE",
+                    12: "EXC_GUARD",
+                    13: "EXC_CORPSE_NOTIFY"}
+
+# From sys/signal.h, as per https://developer.apple.com/documentation/metrickit/mxcrashdiagnostic/3552298-signal?language=objc
+signalTypes = {1: "SIGHUP",
+                2: "SIGINT", 
+                3: "SIGQUIT",
+                4: "SIGILL",
+                5: "SIGTRAP",
+                6: "SIGABRT",
+                7: "SIGPOLL / SIGEMT",
+                8: "SIGFPE",
+                9: "SIGKILL",
+                10: "SIGBUS",
+                11: "SIGSEGV",
+                12: "SIGSYS",
+                13: "SIGPIPE",
+                14: "SIGALRM",
+                15: "SIGTERM",
+                16: "SIGURG",
+                17: "SIGSTOP",
+                18: "SIGTSTP",
+                19: "SIGCONT",
+                20: "SIGCHLD",
+                21: "SIGTTIN",
+                22: "SIGTTOU",
+                23: "SIGIO",
+                24: "SIGXCPU",
+                25: "SIGXFSZ",
+                26: "SIGVTALRM",
+                27: "SIGPROF",
+                28: "SIGWINCH",
+                29: "SIGINFO",
+                30: "SIGUSR1",
+                31: "SIGUSR2"}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--report-path", help="Path to MetricKit diagnostic report")
+parser.add_argument("--symbols-path", help="Path to symbols file, either xcarchive or dSYM")
+parser.add_argument("--binary-name", help="Binary name. Pulled from the file name of the symbols path if not specified.")
+
+args = parser.parse_args()
+
+if not args.report_path or not args.symbols_path:
+    print("Report path and symbols path are required.")
+    exit(1)
+
+jsonPath = args.report_path
+symbolsFilePath = args.symbols_path
+
+binaryName = ""
+if args.binary_name:
+    binaryName = args.binary_name
+else:
+    symbolFileName = symbolsFilePath.split("/")[-1]
+    binaryName = symbolFileName.split(".")[0]
+
+if symbolsFilePath.endswith(".xcarchive"):
+    symbolsFilePath = "{0}/dSYMs/{1}.app.dSYM/Contents/Resources/DWARF/{1}".format(symbolsFilePath, binaryName)
+    
+print("Binary name: {0}".format(binaryName))
+
+if not os.path.exists(symbolsFilePath):
+    print("dSYM path '{0}' does not exist".format(symbolsFilePath))
+    exit(0)
+
+def getDsymUuid(path):
+    uuidResultLine = subprocess.run(["dwarfdump", "--uuid", path], stdout=subprocess.PIPE).stdout.decode("utf-8")
+    dsymUuid = re.search("UUID: ([0-9A-Za-z\-]+?) \(.+", uuidResultLine).group(1)
+    return dsymUuid
+
+print("UUID of specified dSYM is {0}".format(getDsymUuid(symbolsFilePath)))
+
+systemLibPath = ""
+
+def printFrame(root):
+    offset = root["offsetIntoBinaryTextSegment"]
+    originBinaryName = root["binaryName"]
+    originUuid = root["binaryUUID"]
+    sampleCount = 0
+    if "sampleCount" in root:
+        sampleCount = root["sampleCount"]
+
+    dsymPath = ""
+    architecture = "arm64e"
+    if originBinaryName == binaryName:
+        # If the binary name is the one we specified the symbols path for, use that
+        dsymPath = symbolsFilePath
+        architecture = "arm64"
+    elif originBinaryName.startswith("lib") and originBinaryName.endswith(".dylib"):
+        # A binary name like libsystem_kernel.dylib is going to be in <device folder>/usr/lib/system
+        dsymPath = systemLibPath + "usr/lib/system/" + originBinaryName
+    else:
+        # Other binary names like Foundation or UIKitCore are going to be either in <device folder>/System/Library/Frameworks or <device folder>/System/Library/PrivateFrameworks
+        dsymPath = systemLibPath + "System/Library/Frameworks/{0}.framework/{0}".format(originBinaryName)
+        if not os.path.exists(dsymPath):
+            dsymPath = systemLibPath + "System/Library/PrivateFrameworks/{0}.framework/{0}".format(originBinaryName)
+
+    processedLine = False
+    if os.path.exists(dsymPath):
+        dsymUuid = getDsymUuid(dsymPath)
+        if originUuid == dsymUuid:
+            # This is based on this forum post: https://developer.apple.com/forums/thread/681967
+            atosResult = subprocess.run(["atos", "-arch", architecture, "-o", dsymPath, "-l", "0x1", hex(offset)], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+            if sampleCount > 0:
+                # If we have a sample count, this is a cpu diagnostic. Print it sort of like how spindumps are formatted.
+                print("{0}: {1}".format(sampleCount, atosResult))
+            else:
+                # Crash diagnostic or otherwise
+                print(atosResult)
+            processedLine = True
+        else:
+            print("--Warning: UUID of {1} ({2}) does not match expected {3} for {4}".format(dsymPath, dsymUuid, originUuid, originBinaryName))
+    
+    if processedLine == False:
+        print("{0} ({1})".format(originBinaryName, offset))
+
+    if "subFrames" in root:
+        frames = root["subFrames"]
+        for sub in frames:
+            printFrame(sub)
+
+
+def printCallstack(callstackTree):
+    index = 1
+    for stack in callstackTree["callStacks"]:
+        rootFrames = stack["callStackRootFrames"]
+        if len(rootFrames) != 1:
+            print("More than one thing in rootFrames!")
+
+        for root in rootFrames:
+            print('Call stack {0}:'.format(index))
+            printFrame(root)
+            print("")
+            index += 1
+
+def processCrashDiagnostic(diag):
+    meta = diag["diagnosticMetaData"]
+    bundleId = meta["bundleIdentifier"]
+    excType = meta["exceptionType"]
+    appVersion = meta["appVersion"]
+    appBuildVersion = meta["appBuildVersion"]
+    osVersion = meta["osVersion"]
+    excCode = meta["exceptionCode"]
+    signal = meta["signal"]
+
+    print("Symbolicating crash report from {0} {1}.{2}".format(bundleId, appVersion, appBuildVersion))
+
+    exceptionTypeName = "unknown"
+    if excType in exceptionTypes:
+        exceptionTypeName = exceptionTypes[excType]
+
+    print("Exception type: {0}, {1}".format(excType, exceptionTypeName))
+    print("Exception code: {0}".format(excCode))
+
+    signalName = "unknown"
+    if signal in signalTypes:
+        signalName = signalTypes[signal]
+
+    print("Signal: {0}, {1}".format(signal, signalName))
+    print("")
+
+    callstackTree = diag["callStackTree"]
+    printCallstack(callstackTree)
+
+def processDiskDiagnostic(diag):
+    meta = diag["diagnosticMetaData"]
+    bundleId = meta["bundleIdentifier"]
+    appVersion = meta["appVersion"]
+    appBuildVersion = meta["appBuildVersion"]
+    osVersion = meta["osVersion"]
+    writes = meta["writesCaused"]
+
+    print("Symbolicating disk write exception diagnostic from {0} {1}.{2}".format(bundleId, appVersion, appBuildVersion))
+    print("Writes caused: {0}".format(writes))
+
+    callStack = diag["callStackTree"]
+    printCallstack(callStack)
+
+def processCpuDiagnostic(diag):
+    meta = diag["diagnosticMetaData"]
+    bundleId = meta["bundleIdentifier"]
+    appVersion = meta["appVersion"]
+    appBuildVersion = meta["appBuildVersion"]
+    osVersion = meta["osVersion"]
+    totalTime = meta["totalCPUTime"]
+    sampledTime = meta["totalSampledTime"]
+
+    print("Symbolicating CPU exception diagnostic from {0} {1}.{2}".format(bundleId, appVersion, appBuildVersion))
+    print("Total time: {0} of {1}".format(totalTime, sampledTime))
+
+    callStack = diag["callStackTree"]
+    printCallstack(callStack)
+
+with open(jsonPath, 'r') as jsonFile:
+    jsonData = json.loads(jsonFile.read())
+
+    custId = jsonData["customer_id"]
+    timestamp = jsonData["timestamp"]
+    osVersion = jsonData["os_version"]
+    deviceType = jsonData["device_model"]
+    reportDate = datetime.datetime.fromtimestamp(timestamp, datetime.timezone.utc).isoformat()
+
+    print("Customer ID: {0}".format(custId))
+    print("Date of report on device: {0}".format(reportDate))
+    print("Device: {0}, {1}".format(deviceType, osVersion))
+    
+    # Find the folder containing system libraries for this OS version.
+    # Folder names here for modern iOS versions are formatted like `<device model> <OS version> <OS build>`
+    # For example: `iPad13,16 17.1 (21B5045h)`
+    # So we look for a folder containing the OS version from the report
+    startPath = "~/Library/Developer/Xcode/iOS DeviceSupport/"
+    startPath = os.path.expanduser(startPath)
+    for deviceFolder in os.listdir(startPath):
+        if osVersion in deviceFolder:
+            systemLibPath = startPath + deviceFolder + "/Symbols/"
+            print("Found system library path for {0}: {1}".format(osVersion, systemLibPath))
+            break
+
+    if len(systemLibPath) == 0:
+        print("Warning: failed to find system library path for {0}".format(osVersion))
+        
+    print("")
+
+    payload = jsonData["payload"]
+
+    if "crashDiagnostics" in payload:
+        crashDiags = payload["crashDiagnostics"]
+        if len(crashDiags) != 1:
+            print("More than one crashDiagnostics entry!")
+        for diag in crashDiags:
+            processCrashDiagnostic(diag)
+
+    if "diskWriteExceptionDiagnostics" in payload:
+        diskDiags = payload["diskWriteExceptionDiagnostics"]
+        for diag in diskDiags:
+            processDiskDiagnostic(diag)
+
+    if "cpuExceptionDiagnostics" in payload:
+        cpuDiags = payload["cpuExceptionDiagnostics"]
+        for diag in cpuDiags:
+            processCpuDiagnostic(diag)
+    
+
--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # MXSymbolicate
-A Python script to symbolicate MXDiagnosticPayload call stacks
+
+`MXSymbolicate.py` is an example python script for symbolicating call stacks in json-formatted diagnostic reports produced by [MetricKit](https://developer.apple.com/documentation/metrickit).
+
+It's specifically written to deal with the diagnostic reports as saved by the [Olive Tree Bible App](https://apps.apple.com/us/app/bible-app-read-study-daily/id332615624), in which iOS report json is embedded in a root object with some other properties for convenience. The script will not work if given an unmodified MetricKit diagnosic report, but can be modified reasonably easily to do so. It's meant as an example, not as a plug-and-play tool.
+
+Olive Tree's reports look something like this:
+
+```
+{
+    "app_version": "7.15.1.1958",
+    "device_model": "iPhone12,1",
+    "os_version": "17.1.1",
+    "timestamp": "2023-11-22T08:59:34.783Z",
+    "payload": {
+        ...MetricKit payload here...
+    }
+}
+```
+
+They get saved to disk on the device and included when sending diagnostics to our support department.
+
+Caveat emptor: I am not an experienced Python developer, so I'm sure there are lots of things about this script that could be done differently. :-)
+
+## Usage
+
+The simplest usage is to give a report path and symbols file path:
+```
+./MXSymbolicate.py --report-path diagnosticReport.json --symbols-path /path/to/app.dSYM/Contents/Resources/DWARF/app
+```
+
+There's also an optional `--binary-name` argument if the binary name of the app is not the same as the file name of the symbols file:
+
+```
+./MXSymbolicate.py --report-path diagnosticReport.json --symbols-path /path/to/app.dSYM/Contents/Resources/DWARF/app --binary-name MyApp
+```
+
+The `--symbols-path` can also be a path to an `xcarchive` file, in which case the script will drill down to find the symbols file:
+
+```
+./MXSymbolicate.py --report-path diagnosticReport.json --symbols-path /path/to/MyApp.xcarchive
+```
+
+In that example, the binary name will be taken as `MyApp`, and it'll look for a dSYM at `/path/to/MyApp.xcarchive/dSYMs/MyApp.app.dSYM/Contents/Resources/DWARF/MyApp`.
+
+When called in any of these ways, the script will print some metadata from the report and the symbolicated call stacks.
+
+## Symbolicating frames in system libraries
+
+`MXSymbolicate` uses the dSYM provided to it via `--symbols-path` to symbolicate call stack frames in the source app. But it needs separate dSYM files for system libraries like `UIKit` and `libsystem_kernel.dylib`. As it turns out, these are available for any iOS version that's been on a device you've connected to Xcode in `~/Library/Developer/Xcode/iOS DeviceSupport`. Folders there appear to be named according to device and OS version: `<device type> <os version> <os build number>`, ex. `iPad13,16 17.0 (21A329)`.
+
+Via trial and error, I've figured out that:
+
+ - Binaries with names like `libsystem_kernel.dylib` and `libsystem_pthread.dylib` (beginning with `lib` and ending with `.dylib`) are in `iOS DeviceSupport/<device>/Symbols/usr/lib/system/<name>`
+ - Other names like `UIKitCore` and `Foundation` are either in `iOS DeviceSupport/<device>/Symbols/System/Library/` or `.../Symbols/System/PrivateFrameworks`.
+
+The script finds a device folder there that matches the iOS version specified in the report, and then follows those rules to find symbol files for system frameworks it finds in call stack frames.
+
+I imagine there are some frameworks that don't follow those rules, or situations in which those rules don't work - I'll figure that out if and when I run into them in a crash report.
+
+## Useful Resources
+
+There's a surprising lack of documentation on how exactly to use the information in the diagnostic reports. But here's what I used to figure out how to get this far:
+
+ - Introduction of MetricKit at WWDC 2019: [Improving Battery Life and Performance](https://developer.apple.com/videos/play/wwdc2019/417/)
+ - WWDC 2020: [What's New in MetricKit](https://developer.apple.com/videos/play/wwdc2020/10081/)
+ - [Apple documentation on symbolicating](https://developer.apple.com/documentation/xcode/adding-identifiable-symbol-names-to-a-crash-report)
+ - [Apple Developer Forum thread](https://developer.apple.com/forums/thread/681967) on how to plug the values in the call stack tree into `atos`
+ - [Where to find system framework dSYM files](https://www.finik.net/2017/03/20/iOS-Crash-Symbolication-for-dummies-Part-2/)
+
+## Remaining Questions
+
+There are various things I'd still love to know:
+
+ - In a crash diagnostic, how do I know which thread actually crashed?
+ - What does the `callStackPerThread` property in the call stack mean?
+ - What's the purpose of the `address` field in the call stack frame objects?
+ - What situations trigger disk write exceptions and CPU exceptions?
+ - What are the complete rules about where to find dSYM files for system frameworks?
+ - Call stacks are formatted as recursive objects, with each frame having a `subFrames` object. Usually these contain only one object, but sometimes there's more. Why?
+ - Sometimes the `callStackRootFrames` array has more than one object in it. Why?
